066f853a269fe11110d42a5c3a270f81cb108564
Francisco Santamarina <fjsantam@gmail.com>
2017-04-06 12:53:44 -0400

    Filters to Zipcode Level; City Still Outstanding

--- NPO Data/Download and Clean the NCCS Data.R
+++ NPO Data/Download and Clean the NCCS Data.R
@@ -16,12 +16,23 @@ file.remove( "BMF_Aug_2016.zip" )
 ## Load Source Data:
 setwd("~data/extracts")
 dat <- read.csv( "N2s8d2m.csv" )
-str(dat)
+# Data Dictionary for this data is: http://nccsweb.urban.org/PubApps/showDD.php#Business%20Master%20Files
 
-# Load Filter Values:
 
-## Filter Values: Zip Codes
-setwd("~data/extracts")
-zipc <- read.csv( "Zip Codes.csv" )
+
+# Filter Values:
+
+## Load Filter Values: Zip Codes
+zipc <- read.csv( "https://raw.githubusercontent.com/christine-brown/SyracuseLandBank/master/NPO%20Data/Zip%20Codes.csv", 
+                  header = FALSE,
+                  fileEncoding="UTF-8-BOM" )
 names(zipc) <- "zipcode"
 zipc <- zipc$zipcode
+
+## Apply Filter Values: Zip Codes
+### Returns a dataset of rows that have a value for zip5 within the zipc vector
+dat <- dat[ dat$zip5 %in% zipc, ]
+
+
+
+
