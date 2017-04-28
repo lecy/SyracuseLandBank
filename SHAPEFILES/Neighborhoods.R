db884cf1643f3dc3b91a6ecfd9bfde5e1256ef6a
Jesse Lecy <jdlecy@gmail.com>
2017-04-27 18:10:59 -0700

    Add files via upload

---
+++ SHAPEFILES/Neighborhoods.R
@@ -0,0 +1,58 @@
+
+setwd( "C:/Users/jdlecy/Dropbox/02 - CLASSES/02 - MASTERS/09 - DDM II/02 - IND STUDY/03 - SYR Data/Neighborhood Shapefiles" )
+
+library( maptools )
+library( sp )
+library( rgdal )
+library( geojsonio )
+
+
+# Download shapefiles from Community Geography Site
+
+download.file("http://communitygeography.org/wp-content/uploads/2013/09/2015socpa_neighborhoods_utm.zip", "syr_neighborhoods.zip" )
+
+unzip( "syr_neighborhoods.zip" )
+
+file.remove( "syr_neighborhoods.zip" )
+
+
+
+
+syr <- readOGR( dsn=".", layer="2015socpa_neighborhoods_utm" )
+
+plot( syr )
+
+text( getSpPPolygonsLabptSlots(syr), labels=syr$NAME, cex=0.4 )
+
+
+
+# GET AND CHANGE PROJECTION to WGS84
+# http://gis.stackexchange.com/questions/31743/projecting-sp-objects-in-r
+
+is.projected( syr )
+proj4string( syr )
+
+syr <- spTransform( syr, CRS("+proj=longlat +datum=WGS84") )
+proj4string( syr )
+
+
+# WRITE TO GEOJSON FORMAT
+
+geojson_write( syr, geometry="polygon", file="syr_neighborhoods.geojson" )
+
+
+
+
+
+### READ IN FROM GEOJSON FORMAT
+
+library( geojsonio )
+library( sp )
+library( rgdal )
+
+url <- "https://raw.githubusercontent.com/lecy/geojson/master/syr_parcels.geojson"
+syr <- geojson_read( url, method="local", what="sp" )
+
+plot( syr, border="light gray" )
+text( getSpPPolygonsLabptSlots(syr), labels=syr$NAME, cex=0.4 )
+
